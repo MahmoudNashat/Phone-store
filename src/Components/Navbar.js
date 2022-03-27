61d75288a8f3bfb5314bdf00c978c8fb3ae7fbfe
Mahmoud nashat <mahmoudnasshat@gmail.com>
2022-03-27 20:42:36 +0200

    new updates

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from "../logo.svg";
-import {ButtonContainer} from "./Button";
+import { ButtonContainer } from "./Button";
 import styled from 'styled-components';
 export default class Navbar extends Component {
     render() {
         return (
-            <Nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+            <Nav className="navbar navbar-expand-sm navbar-dark px-sm-5 ">
                 <div className="container">
                     <Link to="/">
                         <img src={Logo} alt="store logo" />
                     </Link>
-                    <ul className="navbar-nav ms-5">
+                    <ul className="navbar-nav">
                         <li className="nav-item">
                             <Link to="/" className="nav-link">
                                 Products
                             </Link>
                         </li>
                     </ul>
-                    <Link to="/Cart" className="ms-auto">
+                    <Link to="/Cart">
                         <ButtonContainer>
                             <span>
-                            <i className="fas fa-shopping-cart me-2"></i>
+                                <i className="fas fa-shopping-cart me-2"></i>
                             </span>
                             My Cart
                         </ButtonContainer>
